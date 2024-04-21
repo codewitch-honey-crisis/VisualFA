55a7d056d1fae669fcef93246df87913b539340f
codewitch-honey-crisis <gaztoast@gmail.com>
2024-04-21 00:45:31 -0700

    added scrape example

--- Scratch2/Program.cs
+++ Scratch2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics.Metrics;
+using System.Text;
 
 using VisualFA;
 
@@ -6,37 +7,27 @@ namespace Scratch2
 {
     internal class Program
    {
-		public static IEnumerable<FAMatch> Scrape(HttpClient client, string url, FA lexer, FA[]? blockEnds = null)
-		{
-			using (var msg = new HttpRequestMessage(HttpMethod.Get, url))
+		
+		static void Main()
+        {
+            var expr = FA.Parse(@"https?\://[^\""\';\)]+");
+			var client = new HttpClient();
+			using (var msg = new HttpRequestMessage(HttpMethod.Get, "https://www.google.com"))
 			{
 				using (var resp = client.Send(msg))
 				{
 					using (var reader = new StreamReader(resp.Content.ReadAsStream()))
 					{
-						foreach (var match in lexer.Run(reader, blockEnds))
+						foreach (var match in expr.Run(reader))
 						{
 							if (match.IsSuccess)
 							{
-								yield return match;
+								Console.WriteLine(match.Value);
 							}
 						}
 					}
 				}
 			}
-			
-		}
-		static void Main()
-        {
-            var url = FA.Parse(@"https?\://[^\""\';\)]+");
-			url.RenderToFile(@"..\..\..\url.jpg");
-            var client = new HttpClient();
-			foreach(var match in Scrape(client,"https://www.google.com", url))
-			{
-				Console.WriteLine(match.Value);
-				
-			}
-            
 		}
         static void Main2()
         {
