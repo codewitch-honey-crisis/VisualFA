337c6a97eb53d4662bf9d3b6fac1e2c9d1024e77
codewitch-honey-crisis <gaztoast@gmail.com>
2024-04-21 00:48:19 -0700

    fixed scrape example

--- Scrape/Program.cs
+++ Scrape/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using VisualFA;
+var expr = FA.Parse(@"https?\://[^\"";\)]+");
+var client = new HttpClient();
+using (var msg = new HttpRequestMessage(HttpMethod.Get, "https://www.google.com"))
+{
+	using (var resp = client.Send(msg))
+	{
+		using (var reader = new StreamReader(resp.Content.ReadAsStream()))
+		{
+			foreach (var match in expr.Run(reader))
+			{
+				if (match.IsSuccess)
+				{
+					Console.WriteLine(match.Value);
+				}
+			}
+		}
+	}
+}
--- Scratch2/Program.cs
+++ Scratch2/Program.cs
@@ -7,29 +7,7 @@ namespace Scratch2
 {
     internal class Program
    {
-		
-		static void Main()
-        {
-            var expr = FA.Parse(@"https?\://[^\""\';\)]+");
-			var client = new HttpClient();
-			using (var msg = new HttpRequestMessage(HttpMethod.Get, "https://www.google.com"))
-			{
-				using (var resp = client.Send(msg))
-				{
-					using (var reader = new StreamReader(resp.Content.ReadAsStream()))
-					{
-						foreach (var match in expr.Run(reader))
-						{
-							if (match.IsSuccess)
-							{
-								Console.WriteLine(match.Value);
-							}
-						}
-					}
-				}
-			}
-		}
-        static void Main2()
+        static void Main()
         {
             using(var reader = new StreamReader(@"..\..\..\data.json"))
             {
