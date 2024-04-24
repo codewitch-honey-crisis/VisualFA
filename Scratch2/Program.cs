71892a926a94bff92485f39d569ab79ba64acdc4
codewitch-honey-crisis <gaztoast@gmail.com>
2024-04-24 01:38:46 -0700

    improved demo

--- Scratch2/Program.cs
+++ Scratch2/Program.cs
@@ -12,51 +12,48 @@ namespace Scratch2
 	        0, 1, 10, 4, 48, 57, 65, 90, 95, 95,
 	        97, 122
         };
+		static void _RunIdents(FARunner runner,string[] idents)
+		{
+			for (int i = 0; i < idents.Length; i++)
+			{
+				if(runner is FAStringRunner)
+				{
+					((FAStringRunner)runner).Set(idents[i]);
+				}
+                else
+                {
+					((FATextReaderRunner)runner).Set(new StringReader(idents[i]));
+                }
+                Console.Write("\"");
+				Console.Write(idents[i]);
+				Console.Write("\" is ");
+				if (!_IsMatch(runner))
+				{
+					Console.Write("not ");
+				}
+				Console.WriteLine("an identifier");
+			}
+		}
         static bool _IsMatch(FARunner runner)
         {
 			return runner.NextMatch().IsSuccess && runner.NextMatch().SymbolId == -2;
 		}
 		static void Main()
         {
-            var tableRunner = FA.Run("baz", _StateTable);
-            if(_IsMatch(tableRunner))
-            {
-				Console.WriteLine("Matched identifier");
-			}
-			else
+			var idents = new string[]
 			{
-				Console.WriteLine("Not an identifier");
-			}
-			tableRunner.Set("");
-			if (_IsMatch(tableRunner))
-			{
-				Console.WriteLine("Matched identifier");
-			}
-			else
-			{
-				Console.WriteLine("Not an identifier");
-			}
-
+				"foo",
+				"",
+				"baz",
+				"1bar"
+			};
+			var tableRunner = new FATextReaderDfaTableRunner(_StateTable);
+			_RunIdents(tableRunner, idents);
 			var fa = FA.FromArray(_StateTable);
             fa.RenderToFile(@"..\..\..\ident.jpg");
 			Console.WriteLine(fa.ToString("e"));
-            var stateRunner = fa.Run("foo");
-            if(_IsMatch(stateRunner))
-            {
-                Console.WriteLine("Matched identifier");
-			}
-			else
-			{
-				Console.WriteLine("Not an identifier");
-			}
-			stateRunner.Set("1bar");
-            if (_IsMatch(stateRunner)) 
-			{
-				Console.WriteLine("Matched identifier");
-			} else
-            {
-                Console.WriteLine("Not an identifier");
-            }
+			var stateRunner = new FAStringStateRunner(fa);
+            _RunIdents(stateRunner, idents);
 		}
         static void Main2()
         {
